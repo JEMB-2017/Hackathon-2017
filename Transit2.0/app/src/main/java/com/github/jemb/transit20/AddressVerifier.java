867a6700a0993d1ded985c207c9c0bc186b72dfd
Smiley43210 <smiley43210@gmail.com>
2017-03-11 05:54:40 -1000

    Used textInputLayouts with the EditTexts so that the hints don't fully disappear when a field is filled in, re-laied out components in the ConstraintLayout, added button to test the AddressVerifier with text inputted to the destination field.

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/AddressVerifier.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/AddressVerifier.java
@@ -1,12 +1,12 @@
 package com.github.jemb.transit20;
-import com.google.*;
 
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,30 +15,35 @@ import java.util.List;
  */
 
 public class AddressVerifier {
-    private Geocoder geo;
-    public AddressVerifier(Context context)
-    {
-        geo = new Geocoder(context);
-    }
-    // returns true if the address is valid otherwise returns false
-    boolean isValidAddress(String address)
-    {
-        if(geo.isPresent())
-        {
-            try {
-                List<Address> addressList = geo.getFromLocationName(address, 1);
-                if(!addressList.isEmpty())
-                {
-                    return true;
-                }
-            }
-            catch(java.io.IOException error)
-            {
-                String message = error.getMessage();
-                Log.e("<error>",message);
-            }
+	private Geocoder geo;
 
-        }
-        return false;
-    }
+	public AddressVerifier(Context context) {
+		geo = new Geocoder(context);
+	}
+
+	public List<Address> getAddresses(String locationName) {
+		try {
+			return geo.getFromLocationName(locationName, 10);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+	}
+
+	// returns true if the address is valid otherwise returns false
+	public boolean isValidAddress(String address) {
+		if (Geocoder.isPresent()) {
+			try {
+				List<Address> addressList = geo.getFromLocationName(address, 1);
+				if (!addressList.isEmpty()) {
+					return true;
+				}
+			} catch (java.io.IOException error) {
+				String message = error.getMessage();
+				Log.e("<error>", message);
+			}
+
+		}
+		return false;
+	}
 }
--- Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
@@ -1,18 +1,45 @@
 package com.github.jemb.transit20;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Address;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    //AddressVerifier av = new AddressVerifier(this);
+	AddressVerifier addressVerifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("AddressVerifier", new AddressVerifier(this).isValidAddress("123 lane") + "");
-    }
+		addressVerifier = new AddressVerifier(this);
+
+		Log.i("AddressVerifier", addressVerifier.isValidAddress("123 lane") + "");
+
+//		ArrayList<Address> addressList = (ArrayList<Address>) addressVerifier.getAddresses("Bishop Museum");
+//		for (int i = 0; i < addressList.size(); i++) {
+//			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(0));
+//			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(1));
+//			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(2));
+//			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(3));
+//			Log.i("Address", "[" + i + "] " + addressList.get(i).getFeatureName());
+//		}
+	}
+
+	public void onSubmit(View view) {
+		ArrayList<Address> addressList = (ArrayList<Address>) addressVerifier.getAddresses(((EditText) findViewById(R.id.destinationInput)).getText().toString());
+		for (int i = 0; i < addressList.size(); i++) {
+			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(0));
+			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(1));
+			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(2));
+			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(3));
+			Log.i("Address", "[" + i + "] " + addressList.get(i).getFeatureName());
+		}
+	}
 }
