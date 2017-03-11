977afa67f2ae4b7d23c8a714433ed5d28e8862e9
William Simmons <willjsimmons@gmail.com>
2017-03-11 13:14:25 -1000

    Map is now working

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/MapActivity.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/MapActivity.java
@@ -1,17 +1,35 @@
 package com.github.jemb.transit20;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 
 public class MapActivity extends AppCompatActivity {
+	private GoogleMap map;
+	protected class MapCallback implements OnMapReadyCallback {
+		@Override
+		public void onMapReady(GoogleMap googleMap) {
 
-    private GoogleMap map;
+			map = googleMap;
+		}
+	}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        initMap(new MapCallback());
 
     }
+    private void initMap(OnMapReadyCallback c)
+    {
+        MapFragment fragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mainMap);
+        fragment.getMapAsync(c);
+    }
+
+
 }
