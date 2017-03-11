800ed19325c6ed9f178b4ef52f1f008ce70e82d3
William Simmons <willjsimmons@gmail.com>
2017-03-11 10:24:19 -1000

    Added testing for google play presence

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 		addressVerifier = new AddressVerifier(this);
 
 		Log.i("AddressVerifier", addressVerifier.isValidAddress("123 lane") + "");
+		googleServicesAvailable();
 
 //		ArrayList<Address> addressList = (ArrayList<Address>) addressVerifier.getAddresses("Bishop Museum");
 //		for (int i = 0; i < addressList.size(); i++) {
@@ -41,5 +46,18 @@ public class MainActivity extends AppCompatActivity {
 			Log.i("Address", "[" + i + "] " + addressList.get(i).getAddressLine(3));
 			Log.i("Address", "[" + i + "] " + addressList.get(i).getFeatureName());
 		}
+
+	}
+	public boolean googleServicesAvailable(){
+		GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+		int isAvailable = api.isGooglePlayServicesAvailable(this);
+		if(isAvailable != ConnectionResult.SUCCESS)
+		{
+			int duration = Toast.LENGTH_SHORT;
+			Toast myToast = Toast.makeText(this,"Google play services are not installed on this device",duration);
+			myToast.show();
+			return false;
+		}
+		return true;
 	}
 }
