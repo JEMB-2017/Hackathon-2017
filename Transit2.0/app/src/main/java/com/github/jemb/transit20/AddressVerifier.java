75a7b5b92eab5b56ebd160c63ab26f4024fcc69f
William Simmons <willjsimmons@gmail.com>
2017-03-10 23:07:48 -1000

    Fixed the function about availability

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/AddressVerifier.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/AddressVerifier.java
@@ -1,22 +1,43 @@
 package com.github.jemb.transit20;
 import com.google.*;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.util.Log;
+
+import java.util.List;
+
 
 /**
  * Created by willj on 3/10/2017.
  */
 
 public class AddressVerifier {
-    public AddressVerifier()
+    private Geocoder geo;
+    public AddressVerifier(Context context)
     {
-
+        geo = new Geocoder(context);
     }
     // returns true if the address is valid otherwise returns false
     boolean isValidAddress(String address)
     {
-        if(true)
+        if(geo.isPresent())
         {
-            return true;
+            try {
+                List<Address> addressList = geo.getFromLocationName(address, 1);
+                if(!addressList.isEmpty())
+                {
+                    return true;
+                }
+            }
+            catch(java.io.IOException error)
+            {
+                String message = error.getMessage();
+                Log.e("<error>",message);
+            }
+
         }
         return false;
     }
