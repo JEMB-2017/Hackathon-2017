3fcdf78596c28b3aac4d6636f67a41fec4d6b5e7
William Simmons <willjsimmons@gmail.com>
2017-03-10 21:54:36 -1000

    added address verifier class

---
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/AddressVerifier.java
@@ -0,0 +1,22 @@
+package com.github.jemb.transit20;
+import  com.google.android.gms.maps.*;
+
+/**
+ * Created by willj on 3/10/2017.
+ */
+
+public class AddressVerifier {
+    public AddressVerifier()
+    {
+
+    }
+    // returns true if the address is valid otherwise returns false
+    boolean isValidAddress(String address)
+    {
+        if()
+        {
+
+        }
+        return false;
+    }
+}
