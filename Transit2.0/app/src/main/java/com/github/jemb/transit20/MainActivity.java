c7411b3f6dddf68944dc0ca523dadfb9c6efb006
William Simmons <willjsimmons@gmail.com>
2017-03-11 03:03:24 -1000

    Add test for AddressVerifier class..

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/MainActivity.java
@@ -2,13 +2,17 @@ package com.github.jemb.transit20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    //AddressVerifier av = new AddressVerifier(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("AddressVerifier", new AddressVerifier(this).isValidAddress("123 lane") + "");
     }
 }
