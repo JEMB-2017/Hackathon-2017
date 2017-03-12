bc6aaad485fd42082f57cecd2f3e4a0a7d4670fe
William Simmons <willjsimmons@gmail.com>
2017-03-11 14:03:03 -1000

    map is updaated began work on location services

--- Transit2.0/app/src/main/java/com/github/jemb/transit20/MapActivity.java
+++ Transit2.0/app/src/main/java/com/github/jemb/transit20/MapActivity.java
@@ -1,8 +1,11 @@
 package com.github.jemb.transit20;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -11,11 +14,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MapActivity extends AppCompatActivity {
 	private GoogleMap map;
+	LocationManager locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
 	protected class MapCallback implements OnMapReadyCallback {
 		@Override
 		public void onMapReady(GoogleMap googleMap) {
 
 			map = googleMap;
+			if(checkSelfPermission("android.permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_GRANTED) {
+				map.setMyLocationEnabled(true);
+			}
+			else
+			{
+				Log.e("bad error","sorry the application doesnt have Fine Location access");
+			}
+
 		}
 	}
     @Override
